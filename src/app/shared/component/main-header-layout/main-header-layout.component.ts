6474f9bd9fe3b963199cd4dee6dd3b5a8b6552fa
Inessa <mineevainessa900@gmail.com>
2019-12-16 16:08:49 +0200

    add directory shared/component/main-header-layout

---
+++ src/app/shared/component/main-header-layout/main-header-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header-layout',
+  templateUrl: './main-header-layout.component.html',
+  styleUrls: ['./main-header-layout.component.scss']
+})
+export class MainHeaderLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
