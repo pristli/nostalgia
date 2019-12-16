2758374aee3794eb88e37462b72d47211f3ff801
Inessa <mineevainessa900@gmail.com>
2019-12-16 21:12:35 +0200

    navbar, routing for navbar, image as a background for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainHeaderLayoutComponent} from "./shared/component/main-header-layout/main-header-layout.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {FashionPageComponent} from "./fashion-page/fashion-page.component";
 import {MoviePageComponent} from "./movie-page/movie-page.component";
 import {SweetsPageComponent} from "./sweets-page/sweets-page.component";
 import {ToysPageComponent} from "./toys-page/toys-page.component";
+import {MainLayoutComponent} from "./shared/component/main-layout/main-layout.component";
 
 
 const routes: Routes = [
-  {
-    path:'', component:MainHeaderLayoutComponent, children:[
-      {path:'', redirectTo:'/', pathMatch:'full'},
       {path:'', component: HomePageComponent},
-      {path:'fashion/:id', component: FashionPageComponent},
-      {path:'movie/:id', component: MoviePageComponent},
-      {path:'sweets/:id', component: SweetsPageComponent},
-      {path:'toys/:id', component:ToysPageComponent}
-    ]
-  }
+      {path:'fashion', component: FashionPageComponent},
+      {path:'movie', component: MoviePageComponent},
+      {path:'sweets', component: SweetsPageComponent},
+      {path:'toys', component:ToysPageComponent}
 ];
 
 @NgModule({
