0d2501f55548e2e7c7a66a6a9cc3f17a44ccf692
Inessa <mineevainessa900@gmail.com>
2019-12-16 16:50:15 +0200

    made routes for home-page,movie-page,fashion-page,sweets-page and toys-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainHeaderLayoutComponent} from "./shared/component/main-header-layout/main-header-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+import {FashionPageComponent} from "./fashion-page/fashion-page.component";
+import {MoviePageComponent} from "./movie-page/movie-page.component";
+import {SweetsPageComponent} from "./sweets-page/sweets-page.component";
+import {ToysPageComponent} from "./toys-page/toys-page.component";
 
 
 const routes: Routes = [
   {
     path:'', component:MainHeaderLayoutComponent, children:[
-
+      {path:'', redirectTo:'/', pathMatch:'full'},
+      {path:'', component: HomePageComponent},
+      {path:'fashion/:id', component: FashionPageComponent},
+      {path:'movie/:id', component: MoviePageComponent},
+      {path:'sweets/:id', component: SweetsPageComponent},
+      {path:'toys/:id', component:ToysPageComponent}
     ]
   }
 ];
